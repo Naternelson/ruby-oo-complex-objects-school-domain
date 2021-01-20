4d79b4e3701be52ada2e8eb16492f96320815d7b
Nate <7nater.nelson@gmail.com>
2021-01-19 18:16:06 -0700

    commit tree df7ed07c2fc04a27935009ad85ccd3dba8b265ef

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+class School
+
+    attr_accessor :name
+    attr_accessor :roster
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+    
+    def add_student(name,grade) #Adds a student to the Roster
+        keys = @roster.keys
+
+        if keys.include?(grade)
+            @roster[grade] << name
+        else
+            @roster[grade] = []
+            @roster[grade] << name
+        end
+    end
+    def grade(grade)
+        @roster[grade]
+    end
+    def sort
+        key_array = @roster.keys
+        key_array.each {|key| @roster[key].sort!}
+        @roster
+    end
+end
